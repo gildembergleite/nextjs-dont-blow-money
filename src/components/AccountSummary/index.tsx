1609935f7dc915a162f18b03375aaf1c862a82e2
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-11 20:19:21 -0300

    feat: created hook useSummary

--- src/components/AccountSummary/index.tsx
+++ src/components/AccountSummary/index.tsx
@@ -1,32 +1,15 @@
-import { useTransactions } from '@/hooks/useTransactions'
 import AccountSummaryCard from './account-summary-card'
+import useSummary from '@/hooks/useSummary'
 
 export default function AccountSummary() {
-  const { transactions } = useTransactions()
-  console.log(transactions)
-  const summary = transactions.reduce(
-    (acc, transaction) => {
-      if (transaction.type === 'income') {
-        acc.income += transaction.amount
-        acc.total += transaction.amount
-      } else {
-        acc.outcome += transaction.amount
-        acc.total -= transaction.amount
-      }
-      
-      return acc
-    },
-    {
-      income: 0, outcome: 0, total: 0
-    }
-  )
+  const { income, outcome, total } = useSummary()
 
   return (
     <section className='flex w-full justify-center'>
       <div className='flex w-full max-w-7xl justify-center items-center gap-8 px-12 -mt-20'>
-        <AccountSummaryCard type="income" value={summary.income} />
-        <AccountSummaryCard type="outcome" value={summary.outcome} />
-        <AccountSummaryCard type="total" value={summary.total} />
+        <AccountSummaryCard type="income" value={income} />
+        <AccountSummaryCard type="outcome" value={outcome} />
+        <AccountSummaryCard type="total" value={total} />
       </div>
     </section>
   )
---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,24 @@
+import { useTransactions } from './useTransactions'
+
+export default function useSummary() {
+  const { transactions } = useTransactions()
+  
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.amount
+        acc.total += transaction.amount
+      } else {
+        acc.outcome += transaction.amount
+        acc.total -= transaction.amount
+      }
+      
+      return acc
+    },
+    {
+      income: 0, outcome: 0, total: 0
+    }
+  )
+
+  return summary
+}
