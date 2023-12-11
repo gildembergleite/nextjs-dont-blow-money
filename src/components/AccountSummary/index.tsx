58faf11ac6a402354b0f7f615c7b8657ef552176
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-11 19:16:01 -0300

    feat: add summary reduce

--- src/components/AccountSummary/index.tsx
+++ src/components/AccountSummary/index.tsx
@@ -1,12 +1,34 @@
+'use client'
+
+import { useTransactions } from '@/hooks/useTransactions'
 import AccountSummaryCard from './account-summary-card'
 
 export default function AccountSummary() {
+  const { transactions } = useTransactions()
+  console.log(transactions)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === 'income') {
+        acc.income += transaction.amount
+        acc.total += transaction.amount
+      } else {
+        acc.outcome += transaction.amount
+        acc.total -= transaction.amount
+      }
+      
+      return acc
+    },
+    {
+      income: 0, outcome: 0, total: 0
+    }
+  )
+
   return (
     <section className='flex w-full justify-center'>
       <div className='flex w-full max-w-7xl justify-center items-center gap-8 px-12 -mt-20'>
-        <AccountSummaryCard type="income" value={1500000} />
-        <AccountSummaryCard type="expense" value={480000} />
-        <AccountSummaryCard type="total" value={1020000} />
+        <AccountSummaryCard type="income" value={summary.income} />
+        <AccountSummaryCard type="outcome" value={summary.outcome} />
+        <AccountSummaryCard type="total" value={summary.total} />
       </div>
     </section>
   )
