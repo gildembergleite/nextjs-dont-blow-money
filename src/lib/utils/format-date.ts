71e5252454f6125587930cc21ea221efd6efb401
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-11 20:13:42 -0300

    refactor: rename const

--- src/lib/utils/format-date.ts
+++ src/lib/utils/format-date.ts
@@ -1,11 +1,11 @@
-export function formatDate(data: Date): string {
+export function formatDate(date: Date): string {
   try {
-    const formatoData = new Intl.DateTimeFormat('pt-BR', {
+    const formattedDate = new Intl.DateTimeFormat('pt-BR', {
       year: 'numeric',
       month: '2-digit',
       day: '2-digit'
     })
-    return formatoData.format(data)
+    return formattedDate.format(date)
   } catch (erro) {
     console.error('Erro ao formatar data:', erro)
     return ''
