4bb01be88324fa4d78d54774a173fbd12e9db54b
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-16 13:51:32 -0300

    feat: add react hook form with zod

---
+++ src/components/Header/dialog-form.tsx
@@ -0,0 +1,168 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { Input } from '../ui/input'
+
+import ArrowUpIcon from '../Icons/arrow-up'
+import ArrowDownIcon from '../Icons/arrow-down'
+
+const dialogFormSchema = z.object({
+  description: z.string({
+    invalid_type_error: 'Você deve inserir uma descrição válida.',
+    required_error: 'O campo descrição deve ser preenchido.',
+  }),
+  amount: z.coerce.number({
+    invalid_type_error: 'Você deve inserir apenas números.',
+    required_error: 'Você deve inserir o valor da transação.',
+  }),
+  category: z.string({
+    invalid_type_error: 'Você deve inserir uma categoria válida.',
+    required_error: 'O campo descrição deve ser preenchido.',
+  }),
+  type: z.enum(['income', 'outcome'], {
+    required_error: 'Você precisa escolher uma opção.',
+  })
+})
+
+type DialogFormValues = z.infer<typeof dialogFormSchema>
+
+export function DialogForm() {
+  const form = useForm<DialogFormValues>({
+    resolver: zodResolver(dialogFormSchema)
+  })
+
+  function onSubmit(data: DialogFormValues) {
+    console.log(data)
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4 px-6">
+
+        {/* DESCRIPTION FIELD */}
+        <FormField
+          control={form.control}
+          name="description"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl className='w-full'>
+                <Input
+                  type='text'
+                  placeholder='Descrição da transação...'
+                  className='w-full'
+                  defaultValue={field.value}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )
+          }
+        />
+
+        {/* AMOUNT FIELD */}
+        <FormField
+          control={form.control}
+          name="amount"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl className='w-full'>
+                <Input
+                  type='number'
+                  placeholder='Insira um valor...'
+                  className='w-full'
+                  defaultValue={field.value}
+                  {...field}
+                />
+              </FormControl>
+
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* CATEGORY FIELD */}
+        <FormField
+          control={form.control}
+          name="category"
+          render={({ field }) => (
+            <FormItem className='w-full'>
+              <FormControl className='w-full'>
+                <Input
+                  type='text'
+                  placeholder='Insira um endereço de email válido...'
+                  className='w-full'
+                  defaultValue={field.value}
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        {/* RADIO GROUP TYPES OF PLAN */}
+        <FormField
+          control={form.control}
+          name="type"
+          render={({ field }) => {
+            return (
+              <FormItem className="space-y-1">
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={field.value}
+                  className="flex my-6"
+                >
+                  <FormItem className='w-full'>
+                    <FormLabel className="text-primary [&:has([data-state=unchecked])>div]:hover:bg-background/70 [&:has([data-state=unchecked])>div]:bg-background [&:has([data-state=checked])>div]:border-primary [&:has([data-state=checked])>div]:bg-primary [&:has([data-state=checked])>div]:text-white">
+                      <div className='flex items-center justify-center gap-2 py-4 w-full cursor-pointer rounded-[6px]'>
+                        <FormControl>
+                          <RadioGroupItem className='border-white text-white sr-only' value="income" />
+                        </FormControl>
+                        <div className='w-6 h-6'><ArrowUpIcon /></div>
+                        <span className='text-white'>Entrada</span>
+                      </div>
+                    </FormLabel>
+                  </FormItem>
+
+                  <FormItem className='w-full'>
+                    <FormLabel className="text-destructive [&:has([data-state=unchecked])>div]:hover:bg-background/70 [&:has([data-state=unchecked])>div]:bg-background [&:has([data-state=checked])>div]:border-destructive [&:has([data-state=checked])>div]:bg-destructive [&:has([data-state=checked])>div]:text-white">
+                      <div className='flex items-center justify-center gap-2 py-4 w-full cursor-pointer rounded-[6px]'>
+                        <FormControl>
+                          <RadioGroupItem className='border-white text-white sr-only' value="outcome" />
+                        </FormControl>
+                        <div className='w-6 h-6'><ArrowDownIcon /></div>
+                        <span className='text-white'>Saída</span>
+                      </div>
+                    </FormLabel>
+                  </FormItem>
+
+                </RadioGroup>
+              </FormItem>
+            )
+          }}
+        />
+
+        <Button
+          className='w-full py-6 rounded-[6px] text-white font-bold'
+          type="submit"
+        > 
+          Cadastrar
+        </Button>
+      </form>
+    </Form>
+  )
+}
--- src/components/Header/new-transaction-dialog.tsx
+++ src/components/Header/new-transaction-dialog.tsx
@@ -1,29 +1,16 @@
-'use client'
-
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
   DialogDescription,
-  DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from '@/components/ui/dialog'
-import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import { Input } from '@/components/ui/input'
-import ArrowUpIcon from '../Icons/arrow-up'
-import ArrowDownIcon from '../Icons/arrow-down'
-import { Label } from '../ui/label'
-import { useState } from 'react'
+
+import { DialogForm } from './dialog-form'
 
 export function NewTransactionDialog() {
-  const [selectedRadio, setSelectedRadio] = useState<string | null>(null)
-
-  const handleRadioChange = (value: string) => {
-    setSelectedRadio(value)
-    console.log(selectedRadio)
-  }
   
   return (
     <Dialog>
@@ -32,7 +19,7 @@ export function NewTransactionDialog() {
             Nova transação
         </Button>
       </DialogTrigger>
-      <DialogContent className="sm:max-w-lg bg-card">
+      <DialogContent className="sm:max-w-lg bg-card border-2 border-card">
         <DialogHeader className='ml-6 mt-6'>
           <DialogTitle className='text-2xl'>
             Nova transação
@@ -41,68 +28,7 @@ export function NewTransactionDialog() {
             Insira os dados para cadastrar uma nova transação.
           </DialogDescription>
         </DialogHeader>
-        <div className="space-y-4 px-6">
-          <Input
-            type='text'
-            placeholder='Descrição'
-            id="description"
-          />
-          <Input
-            type='text'
-            placeholder='Preço'
-            id="price"
-          />
-          <Input
-            type='text'
-            placeholder='Categoria'
-            id="category"
-          />
-        </div>
-        <div className='flex w-full px-6 gap-4 mt-2'>
-          <RadioGroup className='flex w-full'>
-            <Button
-              className={`w-full h-full rounded-[6px] p-0 overflow-hidden ${
-                selectedRadio === 'income' ? 'bg-primary' : 'text-primary hover:bg-background/60'
-              }`}
-              onClick={() => handleRadioChange('income')}
-              variant={'secondary'}
-            >
-              <Label
-                className='flex items-center justify-center gap-2 w-full h-full py-4 px-6 cursor-pointer'
-                htmlFor='income'
-              >
-                <RadioGroupItem className='sr-only' value="income" id="income" />
-                <div className='w-6 h-6'><ArrowUpIcon /></div>
-                <span className='text-white'>Entrada</span>
-              </Label>
-            </Button>
-
-            <Button
-              className={`w-full h-full rounded-[6px] p-0 overflow-hidden ${
-                selectedRadio === 'outcome' ? 'bg-destructive' : 'text-destructive hover:bg-background/60'
-              }`}
-              onClick={() => handleRadioChange('outcome')}
-              variant={'secondary'}
-            >
-              <Label
-                className='flex items-center justify-center gap-2 w-full h-full py-4 px-6 cursor-pointer'
-                htmlFor='outcome'
-              >
-                <RadioGroupItem className='sr-only' value="outcome" id="outcome" />
-                <div className='w-6 h-6'><ArrowDownIcon /></div>
-                <span className='text-white'>Saída</span>
-              </Label>
-            </Button>
-          </RadioGroup>
-        </div>
-        <DialogFooter className='px-6 py-2 my-6'>
-          <Button
-            className='w-full h-full py-4 rounded-[6px] text-white font-bold'
-            type="submit"
-          > 
-            Cadastrar
-          </Button>
-        </DialogFooter>
+        <DialogForm />
       </DialogContent>
     </Dialog>
   )
