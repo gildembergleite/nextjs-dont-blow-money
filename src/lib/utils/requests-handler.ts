fc27f3e0e441f5b91438b70e15cfdc56f6da200f
Gildemberg Leite <gildembergleite@gmail.com>
2023-12-16 14:06:00 -0300

    feat: create RequestsHandler class

---
+++ src/lib/utils/requests-handler.ts
@@ -0,0 +1,43 @@
+import { Transaction } from '@/@types/transaction'
+
+export class RequestsHandler {
+  private baseUrl: string
+
+  constructor() {
+    this.baseUrl = process.env.NEXT_PUBLIC_API_URL
+  }
+
+  async get() {
+    try {
+      const response = await fetch(process.env.NEXT_PUBLIC_API_URL)
+      if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.statusText}`)
+      }
+
+      const data = await response.json()
+      return data
+    } catch (err) {
+      const error = err as Error
+      console.error('Erro ao buscar dados:', error.message)
+      throw error
+    }
+  }
+
+  async post(body: Transaction) {
+    try {
+      const response = await fetch(`${this.baseUrl}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      })
+
+      const data = await response.json()
+      return data
+    } catch (error) {
+      console.error('Erro na requisição POST:', error)
+      throw error
+    }
+  }
+}
